a01fd5680c45763287fbf60af97f7d92ef884c75
Mostafa El Bakhti <mel-bakh@c1r6p2.1337.ma>
2026-09-20 17:08:27 +0000

    update

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -26,7 +26,7 @@ function Content( {course} ) {
 
 function Total( {course} ) {
   return (
-    <p>Number of exercises {course.parts[0].exercises + course.parts[0].exercises + course.parts[0].exercises}</p>
+    <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
   )
 }
 
