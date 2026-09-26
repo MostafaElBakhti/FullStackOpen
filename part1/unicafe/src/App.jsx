95dbafe2056fa5f1776bf765dd17cb45f665c1ea
Mostafa El Bakhti <mel-bakh@c1r7p2.1337.ma>
2026-09-26 18:03:09 +0000

    push

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,12 +3,13 @@ import { useState } from 'react'
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
+  console.log(good);
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <button onClick={ () => setGood(good + 1)} > button </button>
     </div>
   )
 }
